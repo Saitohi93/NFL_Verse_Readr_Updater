e4ef3a43e823296e6465dd7969b4d6f9c144050f
Saitohi93 <duke97456@gmail.com>
2026-08-20 16:33:45 -0700

    Add pinned nflreadr installer

---
+++ scripts/install_nflreadr.R
@@ -0,0 +1,34 @@
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+required_version <- package_version("1.5.1")
+
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
+
+installed_version <- if (requireNamespace("nflreadr", quietly = TRUE)) {
+  packageVersion("nflreadr")
+} else {
+  package_version("0")
+}
+
+if (installed_version != required_version) {
+  remotes::install_version(
+    package = "nflreadr",
+    version = as.character(required_version),
+    repos = getOption("repos"),
+    upgrade = "never",
+    dependencies = TRUE
+  )
+}
+
+actual_version <- packageVersion("nflreadr")
+if (actual_version != required_version) {
+  stop(
+    "Expected nflreadr ", required_version,
+    " but installed ", actual_version,
+    call. = FALSE
+  )
+}
+
+message("Installed nflreadr ", actual_version)
